2a5e138c0ccfbde003eb2bbe19316ea5bad71365
vicbradley <vicbradley33@gmail.com>
2024-07-22 16:10:12 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  optimizeFonts: false,
 
   webpack: (config, { isServer, buildId, dev, webpack }) => {
     if (!isServer) {

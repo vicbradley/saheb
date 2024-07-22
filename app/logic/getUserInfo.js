375e08f8b7ebb7ee13f9ffcf074a8a615c8c1336
vicbradley <vicbradley33@gmail.com>
2024-07-22 16:15:26 +0800

    Update getUserInfo.js

--- app/logic/getUserInfo.js
+++ app/logic/getUserInfo.js
@@ -1,6 +1,8 @@
 "use client";
 
 export const getUserInfo = () => {
+  if (typeof window === "undefined") return;
+
   const authData = JSON.parse(localStorage.getItem("auth")) || {};
 
   const { uid, username, profilePicture, email, store } = authData;

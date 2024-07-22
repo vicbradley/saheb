9332c6077650e48d792693dd0e634cdcd57c55ca
vicbradley <vicbradley33@gmail.com>
2024-07-22 19:36:37 +0800

    fix: localStorage is undefined

--- app/logic/getUserInfo.js
+++ app/logic/getUserInfo.js
@@ -1,9 +1,13 @@
 "use client";
 
 export const getUserInfo = () => {
-  const authData = JSON.parse(localStorage.getItem("auth")) || {};
+  if (typeof window !== "undefined") { // Check if running on the client side
+    const authData = JSON.parse(localStorage.getItem("auth")) || {};
 
-  const { uid, username, profilePicture, email, store } = authData;
+    const { uid, username, profilePicture, email, store } = authData;
 
-  return { uid, username, profilePicture, email, store };
-}
+    return { uid, username, profilePicture, email, store };
+  }
+  
+  return {}; // Return an empty object if running on the server side
+};
